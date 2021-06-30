52f34e878e113ae060b359e7d719e368bf883146
DESKTOP-7V8N4AO\rn100 <rn1007@naver.com>
2021-06-30 15:22:57 +0900

    (리눅스)IP주소를 매개변수로 실행하면 32비트 정수 형태로 변환하고 네트워크바이트 순서로 정렬해서 출력하는 프로그램 주소를 변환하는 함수는 inet_aton을 사용했다.

---
+++ inet_aton.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+	struct sockaddr_in addr_inet;
+	
+	if(!inet_aton(argv[1], &addr_inet.sin_addr))
+		error_handling("Conversion error");
+	else
+		printf("Nerwork orderd integer addr: %#x \n", addr_inet.sin_addr.s_addr);
+
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
